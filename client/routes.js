ba880f9e06c87bc4d6095a11e608bb86a558aa5e
Allen Roth <allenroth8888@gmail.com>
2018-06-21 17:55:44 -0400

    can search for key words on socrata, and get links to the dataset

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Upload} from './components'
+import {Login, Signup, UserHome, Upload, SearchBar} from './components'
 import {me} from './store'
 
 /**
@@ -22,6 +22,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/upload" component={Upload} />
+        <Route path="/search" component={SearchBar} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
