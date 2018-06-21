333f9e97980ac3d70db7515009ab8b446f87297a
augusto-proano <tito4gsus@gmail.com>
2018-06-21 12:50:09 -0400

    chart added

---
+++ client/components/dataset.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {LineChart, Line, CartesianGrid, XAxis, YAxis} from 'recharts'
+import {connect} from 'react-redux'
+
+export const Dataset = () => {
+  const data = [
+    {name: 'richard', age: 34, grade: 100},
+    {name: 'hello', age: 45, grade: 80},
+    {name: 'world', age: 84, grade: 70}
+  ]
+  return (
+    <div>
+      <h1>Chart</h1>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="age" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis dataKey="age" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis dataKey="grade" />
+      </LineChart>
+    </div>
+  )
+}
+
+export default connect(Dataset)
