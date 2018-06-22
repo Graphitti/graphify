d553db825f38ac52996460cc48c5bcacf9f6385f
Du <Sampan0103314@gmail.com>
2018-06-22 17:02:42 -0400

    add x and y axies select box for user to choose which columns are serve as x and y axies

--- client/components/upload.js
+++ client/components/upload.js
@@ -19,7 +19,9 @@ export default class Upload extends Component {
     super(props)
     this.state = {
       uploadFile: [],
-      currentX: ""
+      currentX: '',
+      currentY: [],
+      showGraph: false
     }
     this.handleUpload = this.handleUpload.bind(this)
     this.handleXCategory = this.handleXCategory.bind(this)
@@ -45,22 +47,22 @@ export default class Upload extends Component {
     this.setState({currentX: event.target.value})
   }
 
+  handleYCategory = event => {
+    this.setState({currentY: event.target.value})
+  }
+
   render() {
-    // const data = [
-    //   {name: 'richard', age: 34, grade: 100, expenses: 150},
-    //   {name: 'hello', age: 45, grade: 80, expenses: 200},
-    //   {name: 'world', age: 84, grade: 70, expenses: 120}
-    // ]
     const data = this.state.uploadFile
     const xAxis = []
+    const yAxis = []
     for (let k in data[0]) {
       if (typeof data[0][k] === 'string') xAxis.push(k)
+      else yAxis.push(k)
     }
-    console.log("DATASET", this.state.uploadFile)
-    console.log("CATEGORY", this.state.currentX)
+    console.log('DATASET', this.state.uploadFile)
+    console.log('CATEGORY', this.state.currentX)
     return (
       <div className="container">
-        {console.log('upload ......')}
         <CSVReader
           cssClass="react-csv-input"
           label="Select CSV with secret Death Star statistics"
@@ -72,27 +74,46 @@ export default class Upload extends Component {
             <h3>choose x and y please</h3>
             <form>
               <select onChange={this.handleXCategory}>
-                {xAxis.map(xCategory => <option key={xCategory}>{xCategory}</option>)}
+                <option hidden>choose X</option>
+                {xAxis.map(xCategory => (
+                  <option key={xCategory}>{xCategory}</option>
+                ))}
               </select>
-            </form>{' '}
-            <LineChart width={1000} height={1000} data={data}>
-              <Line type="monotone" dataKey="age" stroke="#8884d8" />
-              <Line type="monotone" dataKey="grade" stroke="#82ca9d" />
-              <CartesianGrid strokeDasharray="3 3" stroke="#ccc" />
-              <XAxis dataKey={this.state.currentX} />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-            </LineChart>
-            <BarChart width={1000} height={1000} data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey={this.state.currentX} />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar type="monotone" dataKey="age" fill="#8884d8" />
-              <Bar type="monotone" dataKey="grade" fill="#82ca9d" />
-            </BarChart>
+              <select onChange={this.handleYCategory}>
+                <option hidden>choose Y</option>
+                {yAxis.map(yCategory => (
+                  <option key={yCategory}>{yCategory}</option>
+                ))}
+              </select>
+            </form>
+            {(this.state.currentX && this.state.currentY.length) && (
+              <div>
+                <LineChart width={1000} height={1000} data={data}>
+                  <Line
+                    type="monotone"
+                    dataKey={this.state.currentY}
+                    stroke="#8884d8"
+                  />
+                  <CartesianGrid strokeDasharray="3 3" stroke="#ccc" />
+                  <XAxis dataKey={this.state.currentX} />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                </LineChart>
+                <BarChart width={1000} height={1000} data={data}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey={this.state.currentX} />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar
+                    type="monotone"
+                    dataKey={this.state.currentY}
+                    fill="#8884d8"
+                  />
+                </BarChart>
+              </div>
+            )}
           </div>
         )}
       </div>
