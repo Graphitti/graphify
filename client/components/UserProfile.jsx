32ef4554277b8349a5b5b700e6eecaa836457036
augusto-proano <tito4gsus@gmail.com>
2018-06-29 01:03:01 -0400

    comments removed

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -4,15 +4,6 @@ import {Link} from 'react-router-dom'
 
 const UserProfile = props => {
   const {email, graphs} = props.user
-  // const img =
-  //   'http://www.bbc.co.uk/staticarchive/bb96431eddaea776224930aba8d3f8473227932b.gif'
-
-  // const graphs = [
-  //   {title: 'graph1', thumnail: img},
-  //   {title: 'graph2', thumnail: img},
-  //   {title: 'graph3', thumnail: img},
-  //   {title: 'graph4', thumnail: img}
-  // ]
   const name = email
     ? `${email[0].toUpperCase()}${email.slice(1, email.search('@'))}`
     : ''
