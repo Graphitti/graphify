d8bb0cd9c00329982491b4146df1a032040908a9
Allen Roth <allenroth8888@gmail.com>
2018-06-28 22:53:16 -0400

    removed some console logs

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../store'
 import axios from 'axios'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
     <Link to="/"><h1>GRAPHIFY</h1></Link>
     <nav>
@@ -18,12 +18,12 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           </a>
         </div>
       ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
     </nav>
     <hr />
     {/* can use this for sending requests to aws with dummy data, delete in the future */}
