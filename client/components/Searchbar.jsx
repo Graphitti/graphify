c56e1c0681626341b8c9fbf509ecf09bc1fbc550
Allen Roth <allenroth8888@gmail.com>
2018-06-30 09:53:56 -0400

    search terms and filters show up in urlbar and can be taken from there, and we can filter searches by category

--- client/components/Searchbar.jsx
+++ client/components/Searchbar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import ShowSearchResults from './ShowSearchResults.jsx'
 import history from '../history'
+import { getSocrataCategories, searchSocrataForDatasets } from './utils/socrataFunctions'
 
 export default class SearchBar extends Component {
     constructor(props) {
@@ -11,11 +12,35 @@ export default class SearchBar extends Component {
             results: [],
             search: '',
             showResults: false,
-            submittedSearch: ''
+            submittedSearch: '',
+            searchCategories: [],
+            filter: 'no filter'
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        const {search} = this.props.location;
+        const queryIndex = search.indexOf('?query=');
+        const filterIndex = search.indexOf('&filter=');
+        let filter, searchbar;
+        let end = filterIndex === -1 ? search.length : filterIndex
+        if (filterIndex !== -1) {
+            filter = search.slice(filterIndex + 8).replace(/%20/g, ' ');
+        }
+        if (queryIndex !== -1) {
+            searchbar = search.slice(queryIndex + 7, end).replace(/%20/g, ' ');
+            this.setState({search: searchbar});
+        }
+        getSocrataCategories()
+        .then(categories => {
+            let setFilter = categories.includes(filter) ? filter : 'no filter';
+            this.setState({searchCategories: categories, filter: setFilter})
+        })
+        .catch(console.error)
     }
 
     handleChange(event) {
@@ -23,25 +48,42 @@ export default class SearchBar extends Component {
         this.setState({ search })
     }
 
+    handleFilter(event) {
+        const filter = event.target.value;
+        this.setState({ filter })
+    }
+
     handleSubmit(event) {
         event.preventDefault();
         const {search} = {...this.state};
-        axios.get(
-            `https://api.us.socrata.com/api/catalog/v1?only=datasets&q=${search}`
-        )
-            .then(res => {
-                this.setState({results: res.data, showResults: true, submittedSearch: search});
-                history.push('/search');
-            })
-            .catch(console.error)
+        const filter = this.state.filter === 'no filter' ? '' : this.state.filter;
+        searchSocrataForDatasets(search, filter)
+        .then(results => {
+            this.setState({results, showResults: true, submittedSearch: search});
+            history.push(`/search?query=${search}&filter=${filter}`);
+        })
+        .catch(console.error)
     }
 
     render() {
-        const { results, submittedSearch } = this.state;
+        const { results, submittedSearch, searchCategories } = this.state;
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleChange} name="searchbar" />
+                    <input onChange={this.handleChange} name="searchbar" value={this.state.search} />
+                    {
+                        searchCategories.length > 0 &&
+                        <select onChange={this.handleFilter} value={this.state.filter}>
+                            <option value="no filter">no filter</option>
+                            {
+                                searchCategories.map(category => (
+                                    <option value={category} key={category}>
+                                        {category}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    }
                     <button type="submit">Search</button>
                 </form>
                 {this.state.showResults &&
