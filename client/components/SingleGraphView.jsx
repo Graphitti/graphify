b4dbab37fe28e0b373d6ce73545c4ea939d804da
Allen Roth <allenroth8888@gmail.com>
2018-07-01 19:08:50 -0400

    able to download a file with image or the code for the image

--- client/components/SingleGraphView.jsx
+++ client/components/SingleGraphView.jsx
@@ -21,6 +21,7 @@ import { HuePicker } from 'react-color'
 import history from '../history'
 import crypto from 'crypto'
 import axios from 'axios'
+import FileSaver from 'file-saver'
 
 axios.defaults.baseURL = 'http://localhost:8080';
 
@@ -29,18 +30,45 @@ class SingleGraphView extends Component {
     super(props)
     this.state = {
       legend: -1,
-      edit: false
+      edit: false,
+      svgDisplay: false
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleChangeColor = this.handleChangeColor.bind(this)
     this.handleClone = this.handleClone.bind(this)
+    this.exportChart = this.exportChart.bind(this)
+    this.exportSVG = this.exportSVG.bind(this)
   }
 
   componentDidMount() {
     const { graphId } = this.props.match.params
     const { getGraphId } = this.props
     getGraphId(graphId)
+  }
+  
+  // Exports the graph as embedded JS or PNG
+  exportChart(asSVG) {
+    // A Recharts component is rendered as a div that contains namely an SVG
+    // which holds the chart. We can access this SVG by calling upon the first child/
+    // let chartSVG = ReactDOM.findDOMNode(this.currentChart).children[0];
+    let chartSVG = document.getElementById('current-chart').children[0];
+    if (asSVG) {
+      let svgURL = new XMLSerializer().serializeToString(chartSVG);
+      let svgBlob = new Blob([svgURL], { type: "image/svg+xml;charset=utf-8" });
+      FileSaver.saveAs(svgBlob, this.state.uuid + ".svg");
+    } else {
+      let svgBlob = new Blob([chartSVG.outerHTML], { type: "text/html;charset=utf-8" });
+      FileSaver.saveAs(svgBlob, this.state.uuid + ".html");
+    }
+  }
+
+  exportSVG() {
+    let chartSVG = document.getElementById('current-chart').children[0];
+    let input = document.getElementById('svg-copy');
+    this.state.svgDisplay = true;
+    let svgURL = new XMLSerializer().serializeToString(chartSVG);
+    input.value = svgURL;
   }
 
   handleChange(event) {
@@ -72,7 +100,7 @@ class SingleGraphView extends Component {
       .replace(/\//g, '7');
     console.log('graphId', graphId)
     const { currentX, currentY, title, xAxisName, yAxisName, colors, graphType } = this.props.graphSettings;
-    const {awsId, name } = this.props.dataset;
+    const { awsId, name } = this.props.dataset;
     axios.post(`api/graphs/${graphId}`, {
       xAxis: currentX,
       yAxis: currentY,
@@ -85,11 +113,11 @@ class SingleGraphView extends Component {
       datasetName: name,
       awsId
     })
-    .then(() => {
-      this.props.history.push(`/graph-dataset/customize/${graphId}`);
-      location.reload();
-    })
-    .catch(console.error)
+      .then(() => {
+        this.props.history.push(`/graph-dataset/customize/${graphId}`);
+        location.reload();
+      })
+      .catch(console.error)
   }
 
   render() {
@@ -97,7 +125,12 @@ class SingleGraphView extends Component {
 
     return (
       <div>
-        <div>
+        {/* this is the code for exporting the image */}
+        <button onClick={() => this.exportChart()}>Download Image</button>
+        <button onClick={() => this.exportSVG()}>Get SVG</button>
+        <input id="svg-copy" style={{height: "30px", width: "400px"}}></input>
+
+        <div id="current-chart">
           {(function () {
             switch (graphType) {
               case 'Line':
@@ -184,5 +217,8 @@ const mapDispatch = dispatch => ({
     dispatch(fetchAndSetDataFromS3(graphId))
   }
 })
+
+
+
 
 export default connect(mapState, mapDispatch)(SingleGraphView)
